48d80356b7a0060a6d42bf66e3955910f975794a
Matt Gleeson <dev@mattgleeson>
2025-03-17 07:13:03 +1000

    updated settings to fix link to site policies admin page

--- settings.php
+++ settings.php
@@ -99,10 +99,11 @@ $settings->add(new \admin_setting_configcheckbox('auth_azureb2c/forceredirect', 
 
 // Silent login mode (bypass SSO login page for already logged in user)
 $configkey = new lang_string('cfg_silentloginmode_key', 'auth_azureb2c');
-$configdesc = new lang_string('cfg_silentloginmode_desc', 'auth_azureb2c');
-$configdefault = '0';
 $forceloginconfigurl = new moodle_url('/admin/settings.php', ['section' => 'sitepolicies']);
+$configdesc = new lang_string('cfg_silentloginmode_desc', 'auth_azureb2c', $forceloginconfigurl->out(false));
+$configdefault = '0';
 $settings->add(new admin_setting_configcheckbox('auth_azureb2c/silentloginmode', $configkey, $configdesc, $configdefault));
+
 
 $configkey = new lang_string('cfg_userrestrictions_key', 'auth_azureb2c');
 $configdesc = new lang_string('cfg_userrestrictions_desc', 'auth_azureb2c');
